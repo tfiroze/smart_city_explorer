eba79ef5c56a3fe42ae1b89882b2ce628fc0b5a6
Jaeger T <comp308302@gmail.com>
2023-06-29 18:22:51 +0100

    Update Tags
    Testing display. Need to link to our data instead of static/manual selection.

--- flask_react/frontend/src/views/Questionnaire.tsx
+++ flask_react/frontend/src/views/Questionnaire.tsx
@@ -4,6 +4,7 @@ import {
   Paper,
   Typography,
   Box,
+  Button,
 } from "@mui/material";
 import dayjs from 'dayjs';
 import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
@@ -14,12 +15,12 @@ import { DesktopTimePicker } from '@mui/x-date-pickers/DesktopTimePicker';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 
-// will link to our data...
-const data = [
-  { id: 1, tag: "Tag 1" },
-  { id: 2, tag: "Tag 2" },
-  { id: 3, tag: "Tag 3" },
-  // this is just placeholders for now
+// Representing Venue type data from BestTime.app yarra yarra
+const venueTypes = [
+  { id: 1, tag: "Restaurant", venueType: "restaurant" },
+  { id: 2, tag: "Cafe", venueType: "cafe" },
+  { id: 3, tag: "Bar", venueType: "bar" },
+  // More venue types to be linked to data instead of adding manually
 ];
 
 export const Questionnaire = () => {
@@ -29,21 +30,31 @@ export const Questionnaire = () => {
     setSelectedTags(value);
   };
 
+  const filterVenuesByTags = () => {
+    const selectedVenueTypes = venueTypes.filter((venue) =>
+      selectedTags.includes(venue.tag)
+    );
+    // Use the selectedVenueTypes to filter the venues in your app
+    console.log(selectedVenueTypes);
+  };
+
   return (
     <Paper
       style={{
         overflow: "scroll",
         maxHeight: "100vh",
+        padding: "20px",
       }}
+      elevation={3}
     >
       <Grid container style={{ position: "relative" }}>
-        <Grid item xs={12} sm={12} md={12} lg={12} style={{ padding: "10px" }}>
+        <Grid item xs={12}>
           <Typography
             variant="h4"
             align="center"
             color="textSecondary"
             gutterBottom
-            style={{ margin: "20px 0" }}
+            style={{ marginBottom: "20px" }}
           >
             Plan a New Trip
           </Typography>
@@ -54,38 +65,46 @@ export const Questionnaire = () => {
                 'TimePicker'
               ]}
             >
-              <Grid container spacing={2}>
+              <Grid container spacing={2} alignItems="center">
                 <Grid item xs={12} md={6}>
                   <DemoItem label="Select Date">
-                    <DesktopDatePicker defaultValue={dayjs('2022-04-17')} 
-                      // selectedSections={()=>{console.log('Add your selectedSections value here')}}
-                      // onSelectedSectionsChange={()=>{console.log('Add your handleSectionsChange function here')}}  
-                    />
+                    <DesktopDatePicker defaultValue={dayjs()} />
                   </DemoItem>
                 </Grid>
                 <Grid item xs={12} md={6}>
                   <DemoItem label="Select Time">
-                    <DesktopTimePicker defaultValue={dayjs('2022-04-17T15:30')} />
+                    <DesktopTimePicker defaultValue={dayjs()} />
                   </DemoItem>
                 </Grid>
                 <Grid item xs={12}>
-                  <DemoItem label="Select Interests">
+                  <DemoItem label="Interests">
                     <Autocomplete
                       multiple
                       id="tags-standard"
-                      options={data.map((item) => item.tag)}
+                      options={venueTypes.map((item) => item.tag)}
                       value={selectedTags}
                       onChange={handleTagChange}
                       renderInput={(params) => (
                         <TextField
                           {...params}
-                          variant="standard"
+                          variant="outlined"
                           label="Select Categories"
                           placeholder="Tags"
                         />
                       )}
                     />
                   </DemoItem>
+                </Grid>
+                <Grid item xs={12}>
+                  <Box textAlign="center">
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={filterVenuesByTags}
+                    >
+                      Generate Itinerary
+                    </Button>
+                  </Box>
                 </Grid>
               </Grid>
             </DemoContainer>
