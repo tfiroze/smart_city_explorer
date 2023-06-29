8f447777e3dbb5ee4247bd421d0b9dd8287c7571
Jaeger T <comp308302@gmail.com>
2023-06-29 19:08:07 +0100

    Meh

--- flask_react/frontend/src/views/Questionnaire.tsx
+++ flask_react/frontend/src/views/Questionnaire.tsx
@@ -5,6 +5,8 @@ import {
   Typography,
   Box,
   Button,
+  Container,
+  styled,
 } from "@mui/material";
 import dayjs from 'dayjs';
 import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
@@ -15,13 +17,26 @@ import { DesktopTimePicker } from '@mui/x-date-pickers/DesktopTimePicker';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 
-// Representing Venue type data from BestTime.app yarra yarra
 const venueTypes = [
-  { id: 1, tag: "Restaurant", venueType: "restaurant" },
-  { id: 2, tag: "Cafe", venueType: "cafe" },
-  { id: 3, tag: "Bar", venueType: "bar" },
-  // More venue types to be linked to data instead of adding manually
+  { id: 1, tag: "Tourist Attraction", venueType: "tourist_attraction" },
+  { id: 2, tag: "Historical Landmark", venueType: "historical_landmark" },
+  { id: 3, tag: "Scenic Spot", venueType: "scenic_spot" },
+  // Add more venue types here
 ];
+
+const QuestionnairePaper = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(4),
+  marginTop: theme.spacing(4),
+}));
+
+const QuestionnaireTitle = styled(Typography)(({ theme }) => ({
+  marginBottom: theme.spacing(3),
+  color: theme.palette.primary.main,
+}));
+
+const QuestionnaireButton = styled(Button)(({ theme }) => ({
+  marginTop: theme.spacing(3),
+}));
 
 export const Questionnaire = () => {
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
@@ -39,78 +54,63 @@ export const Questionnaire = () => {
   };
 
   return (
-    <Paper
-      style={{
-        overflow: "scroll",
-        maxHeight: "100vh",
-        padding: "20px",
-      }}
-      elevation={3}
-    >
-      <Grid container style={{ position: "relative" }}>
-        <Grid item xs={12}>
-          <Typography
-            variant="h4"
-            align="center"
-            color="textSecondary"
-            gutterBottom
-            style={{ marginBottom: "20px" }}
+    <Container maxWidth="sm">
+      <QuestionnairePaper elevation={3}>
+        <QuestionnaireTitle variant="h4" align="center">
+          Plan a New Trip
+        </QuestionnaireTitle>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer
+            components={[
+              'DesktopDatePicker',
+              'TimePicker'
+            ]}
           >
-            Plan a New Trip
-          </Typography>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer
-              components={[
-                'DesktopDatePicker',
-                'TimePicker'
-              ]}
-            >
-              <Grid container spacing={2} alignItems="center">
-                <Grid item xs={12} md={6}>
-                  <DemoItem label="Select Date">
-                    <DesktopDatePicker defaultValue={dayjs()} />
-                  </DemoItem>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <DemoItem label="Select Time">
-                    <DesktopTimePicker defaultValue={dayjs()} />
-                  </DemoItem>
-                </Grid>
-                <Grid item xs={12}>
-                  <DemoItem label="Interests">
-                    <Autocomplete
-                      multiple
-                      id="tags-standard"
-                      options={venueTypes.map((item) => item.tag)}
-                      value={selectedTags}
-                      onChange={handleTagChange}
-                      renderInput={(params) => (
-                        <TextField
-                          {...params}
-                          variant="outlined"
-                          label="Select Categories"
-                          placeholder="Tags"
-                        />
-                      )}
-                    />
-                  </DemoItem>
-                </Grid>
-                <Grid item xs={12}>
-                  <Box textAlign="center">
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={filterVenuesByTags}
-                    >
-                      Generate Itinerary
-                    </Button>
-                  </Box>
-                </Grid>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item xs={12} sm={6}>
+                <DemoItem label="Select Date">
+                  <DesktopDatePicker defaultValue={dayjs()} />
+                </DemoItem>
               </Grid>
-            </DemoContainer>
-          </LocalizationProvider>
-        </Grid>
-      </Grid>
-    </Paper>
+              <Grid item xs={12} sm={6}>
+                <DemoItem label="Select Time">
+                  <DesktopTimePicker defaultValue={dayjs()} />
+                </DemoItem>
+              </Grid>
+              <Grid item xs={12}>
+                <DemoItem label="Interests">
+                  <Autocomplete
+                    multiple
+                    id="tags-standard"
+                    options={venueTypes.map((item) => item.tag)}
+                    value={selectedTags}
+                    onChange={handleTagChange}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        variant="outlined"
+                        label="Select Categories"
+                        placeholder="Tags"
+                      />
+                    )}
+                  />
+                </DemoItem>
+              </Grid>
+              <Grid item xs={12}>
+                <Box textAlign="center">
+                  <QuestionnaireButton
+                    variant="contained"
+                    color="primary"
+                    onClick={filterVenuesByTags}
+                  >
+                    Generate Itinerary
+                  </QuestionnaireButton>
+                </Box>
+              </Grid>
+            </Grid>
+          </DemoContainer>
+        </LocalizationProvider>
+      </QuestionnairePaper>
+    </Container>
   );
 };
